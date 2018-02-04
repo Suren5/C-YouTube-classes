5e0c0d614e9126d6191eb705ae3e4e52ed2a0ae4
Suren Movsisyan <suren@movsisyan.dk>
2018-02-04 22:59:11 +0400

    Arrays

---
+++ Arrays/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = new int[10];
+            // x[0] ..... x[9]
+            
+            for (int i = 0; i < x.Length; i++)
+            {
+                x[i] = i * i;
+            }
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.Write(x[i] + " ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
