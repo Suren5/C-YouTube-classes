a517fa17cbbcb469279580f42cd9694f43a4d7c0
Suren Movsisyan <suren@movsisyan.dk>
2018-02-04 21:19:30 +0400

    IFELSEProblem2

---
+++ IFELSEproblem2/IFELSEproblem2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFELSEproblem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.Write("Mutqagreq x - ");
+            x = int.Parse(Console.ReadLine());
+
+            Console.Write("Mutqagreq y - ");
+            y = int.Parse(Console.ReadLine());
+
+            Console.Write("Mutqagreq z - ");
+            z = int.Parse(Console.ReadLine());
+
+            if(x == y || y == z || z == x)
+            {
+                Console.WriteLine("Havasar tver kan")
+            }
+            else
+            {
+                Console.WriteLine("Havasar tver chkan")
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
